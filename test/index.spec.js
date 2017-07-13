2faec4affec9fc86351ac8c5fe0b840024fe0ad9
Patrick Paskaris <Patrick.Paskaris@D2L.com>
2017-07-13 15:33:04 -0400

    Add some preliminary tests

---
+++ test/index.spec.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const Promise = require('bluebird');
+const __sinon__ = require('sinon');
+const expect = require('chai').expect;
+const nock = require('nock');
+
+const LandlordClient = require('../src/index');
+const data = {
+	endpoint: 'http://landlord.localhost',
+	tenantId: '88ce2351-2eda-40ba-8774-d4d46f0d2d1a',
+	domain: 'brightspace.localhost',
+	scheme: 'http',
+	isHttpSite: true,
+	tenantUrl: 'http://brightspace.localhost/',
+	maxAge: 3600
+};
+
+describe('LandlordClient', function() {
+	let sandbox = __sinon__.sandbox.create();
+
+	beforeEach(function() {
+		sandbox = __sinon__.sandbox.create();
+	});
+
+	afterEach(function() {
+		sandbox.verifyAndRestore();
+	});
+
+	it('looks up tenant id when not cached', function() {
+		const getRequest = nock(data.endpoint)
+			.get('/v1/tenants')
+			.query({ domain: data.domain })
+			.reply(200, [{
+				tenantId: data.tenantId,
+				domain: data.domain
+			}]);
+
+		const cacheProto = LandlordClient.LRULandlordCache.prototype;
+		const getTenantIdLookupStub =
+			sandbox.stub(cacheProto, 'getTenantIdLookup');
+		getTenantIdLookupStub
+			.withArgs(data.domain)
+			.returns(Promise.reject(new Error('Not Found')));
+		const cacheTenantIdLookupStub =
+			sandbox.stub(cacheProto, 'cacheTenantIdLookup');
+		cacheTenantIdLookupStub
+			.withArgs(data.domain, data.tenantId)
+			.returns(Promise.resolve());
+
+		const instance = new LandlordClient({ endpoint: data.endpoint });
+		return expect(instance.lookupTenantId(data.domain))
+			.to.eventually
+			.equal(data.tenantId)
+			.then(function() {
+				getRequest.done();
+			});
+	});
+
+	it('does not look up tenant id when cached', function() {
+		const getRequest = nock(data.endpoint);
+
+		const cacheProto = LandlordClient.LRULandlordCache.prototype;
+		const getTenantIdLookupStub =
+			sandbox.stub(cacheProto, 'getTenantIdLookup');
+		getTenantIdLookupStub
+			.withArgs(data.domain)
+			.returns(Promise.resolve(data.tenantId));
+
+		const instance = new LandlordClient({ endpoint: data.endpoint });
+		return expect(instance.lookupTenantId(data.domain))
+			.to.eventually
+			.equal(data.tenantId)
+			.then(function() {
+				getRequest.done();
+			});
+	});
+
+	it('looks up tenant info when not cached', function() {
+		const getRequest = nock(data.endpoint)
+			.get('/v1/tenants/' + data.tenantId)
+			.reply(200, {
+				tenantId: data.tenantId,
+				domain: data.domain + '/',
+				isHttpSite: data.isHttpSite
+			}, {
+				'Cache-Control': 'max-age=' + data.maxAge
+			});
+
+		const cacheProto = LandlordClient.LRULandlordCache.prototype;
+		const getTenantUrlLookupStub =
+			sandbox.stub(cacheProto, 'getTenantUrlLookup');
+		getTenantUrlLookupStub
+			.withArgs(data.tenantId)
+			.returns(Promise.reject(new Error('Not Found')));
+		const cacheTenantUrlLookupStub =
+			sandbox.stub(cacheProto, 'cacheTenantUrlLookup');
+		cacheTenantUrlLookupStub
+			.withArgs(data.tenantId, data.tenantUrl, data.maxAge)
+			.returns(Promise.resolve());
+
+		const instance = new LandlordClient({ endpoint: data.endpoint });
+		return expect(instance.lookupTenantUrl(data.tenantId))
+			.to.eventually
+			.equal(data.tenantUrl)
+			.then(function() {
+				getRequest.done();
+			})
+			.then(function() {
+				expect(cacheTenantUrlLookupStub.calledOnce).to.be.true;
+			});
+	});
+
+	it('does not look up tenant info when cached', function() {
+		const getRequest = nock(data.endpoint);
+
+		const cacheProto = LandlordClient.LRULandlordCache.prototype;
+		const getTenantUrlLookupStub =
+			sandbox.stub(cacheProto, 'getTenantUrlLookup');
+		getTenantUrlLookupStub
+			.withArgs(data.tenantId)
+			.returns(Promise.resolve(data.tenantUrl));
+		const cacheTenantUrlLookupSpy =
+			sandbox.stub(cacheProto, 'cacheTenantUrlLookup');
+
+		const instance = new LandlordClient({ endpoint: data.endpoint });
+		return expect(instance.lookupTenantUrl(data.tenantId))
+			.to.eventually
+			.equal(data.tenantUrl)
+			.then(function() {
+				getRequest.done();
+			})
+			.then(function() {
+				expect(cacheTenantUrlLookupSpy.notCalled).to.be.true;
+			});
+	});
+});
